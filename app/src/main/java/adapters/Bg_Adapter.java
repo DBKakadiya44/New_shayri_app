0a2d4d9d51b31209e70512fccd6325baf07ce23b
DBkakadiya44 <deepkakadiya2002>
2023-05-15 08:04:32 +0530

    third commit

--- app/src/main/java/adapters/Bg_Adapter.java
+++ app/src/main/java/adapters/Bg_Adapter.java
@@ -1,20 +1,26 @@
 package adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.example.shayri_app.Background;
+import com.example.shayri_app.R;
 
 public class Bg_Adapter extends BaseAdapter {
 
     Background background; int[] colorarr;
+
     public Bg_Adapter(Background background, int[] colorarr) {
+        this.background = background;
+        this.colorarr = colorarr;
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return colorarr.length;
     }
 
     @Override
@@ -30,7 +36,10 @@ public class Bg_Adapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        convertView = LayoutInflater.from(background).inflate(R.layout.bg_grid_color_item,parent,false);
 
+        TextView textView = convertView.findViewById(R.id.background);
+        textView.setBackgroundColor(colorarr[position]);
         return convertView;
     }
 }
--- app/src/main/java/com/example/shayri_app/Background.java
+++ app/src/main/java/com/example/shayri_app/Background.java
@@ -24,6 +24,7 @@ public class Background extends AppCompatActivity
 
         gridView = findViewById(R.id.bg_gridview);
         adapter = new Bg_Adapter(Background.this,colorarr);
+        gridView.setAdapter(adapter);
 
     }
 }
